db6c3c0c1d7191bd8fb554b4bd8bd754f5ce056a
pow2 <plamenprangov@gmail.com>
2015-10-16 18:09:07 +0300

    Create PrintCharacters.java

---
+++ Intro-Java-Homework/PrintCharacters.java
@@ -0,0 +1,21 @@
+package com.softuni;
+
+/**
+ * Created by pow2 on 15.10.2015 y..
+ *
+ * Print the characters from ‘a’ to ‘z’ on the console on a single line, separated by a space.
+ * Use a for-loop.
+ * Note: you can directly declare and increment char in the for-loop. for (char c = ‘a’; …)
+ */
+public class PrintCharacters {
+    private void Print(){
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            System.out.print(c+" ");
+        }
+        System.out.print("\n");
+    }
+    public void Start() {
+        this.Print();
+    }
+}
