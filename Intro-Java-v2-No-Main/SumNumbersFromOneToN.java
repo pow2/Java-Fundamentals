eef7d0d0a85c9377532d5c8c7b2f6c63ee7a4ade
Plamen Prangov <plamenprangov@gmail.com>
2015-10-20 21:25:10 +0300

    Create SumNumbersFromOneToN.java

---
+++ Intro-Java-v2-No-Main/SumNumbersFromOneToN.java
@@ -0,0 +1,36 @@
+package com.softuniv2;
+
+import java.util.Scanner;
+
+/**
+ * Created by pow2 on 15.10.2015 y.
+ *
+ * Create a Java program that reads a number N
+ * from the console and calculates the sum of all
+ * numbers from 1 to N (inclusive).
+ */
+public class SumNumbersFromOneToN {
+
+    private static void print(int i){
+        int k = i;
+        for (int l=i-1; l > 0; l--)
+        {
+            k+=l;
+        }
+        System.out.print(k+"\n");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("\nEnter N : ");
+        String line = in.nextLine();
+        System.out.print("\n");
+        int result;
+        try {
+            result = Integer.parseInt(line);
+            print(result);
+        } catch (NumberFormatException ex) {
+            System.out.println("Incorrect input");
+        }
+    }
+}
