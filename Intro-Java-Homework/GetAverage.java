b0ec0fdad4513ff857ef75fbf0f41abc9f624749
Plamen Prangov <plamenprangov@gmail.com>
2015-10-20 21:14:59 +0300

    syntax fix

--- Intro-Java-Homework/GetAverage.java
+++ Intro-Java-Homework/GetAverage.java
@@ -1,6 +1,5 @@
 package com.softuni;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -16,27 +15,28 @@ import java.util.Scanner;
  */
 public class GetAverage {
 
- private float Avg3(float a, float b, float c){
+ private float avg3(float a, float b, float c){
+
      return (a+b+c)/3;
  }
 
-    private void UI() throws IOException {
+    private void uI() {
         Scanner in = new Scanner(System.in);
-        System.out.print("\nEnter \"a,b,c\" : ");
+        System.out.print("\nEnter \"a b c\" : ");
         String line = in.nextLine();
         System.out.print("\n");
-        String[] st= line.split(",");
+        String[] st= line.split(" ");
         try {
-            float avg = Avg3(Float.parseFloat(st[0]), Float.parseFloat(st[1]), Float.parseFloat(st[2]));
+            float avg = avg3(Float.parseFloat(st[0]), Float.parseFloat(st[1]), Float.parseFloat(st[2]));
             System.out.printf("The average value is : %1$.2f", avg);
             System.out.printf("\n");
         } catch (Exception e){
-            System.out.println("\n" + e + "\n");
+            System.out.println(e + "\n");
         }
     }
 
-    public void Start() throws IOException {
-        this.UI();
+    public void start() {
+        this.uI();
     }
 
 }
